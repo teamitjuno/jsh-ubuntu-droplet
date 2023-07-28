3253418a3e294f2e66cf8638c8cbcc2131761c30
SamIvanov7 <sam1206morfey@gmail.com>
2023-07-29 01:47:50 +0200

    update

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ SERVICE_APPS = [
     "crispy_forms",
     "shared",
     "storages",
+    'corsheaders',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + SERVICE_APPS
@@ -66,11 +67,15 @@ AUTH_USER_MODEL = "authentication.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     # "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+CORS_ALLOWED_ORIGINS = [
+    "https://jsh-home.app",
 ]
 
 ROOT_URLCONF = "config.urls"
