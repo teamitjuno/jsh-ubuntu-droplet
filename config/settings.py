f042176a0c612f0ef3c4d3c34e49459c90d6ab21
SamIvanov7 <sam1206morfey@gmail.com>
2023-07-29 02:19:24 +0200

    update

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,9 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = "authentication.User"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
