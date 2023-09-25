7be7b391f753c0373d6f58ff76fe3a488a680fb2
SamIvanov7 <sam1206morfey@gmail.com>
2023-09-25 10:52:41 +0200

    update

--- vertrieb_interface/models.py
+++ vertrieb_interface/models.py
@@ -951,13 +951,12 @@ class VertriebAngebot(TimeStampMixin):
     @property
     def restenergie(self):
         rest = 0
-        try:
-            if float(self.verbrauch):
-                rest += float(self.verbrauch) - float(self.nutz_energie)
-        except:
-            self.verbrauch = 15000.0
+        
+        if float(self.verbrauch):
             rest += float(self.verbrauch) - float(self.nutz_energie)
-        return rest
+            return rest
+        else:
+            return 0
 
     @property
     def rest_strom_preis(self):
@@ -979,7 +978,8 @@ class VertriebAngebot(TimeStampMixin):
     @property
     def einsp_pro_jahr(self):
         if self.modulsumme_kWp <= 10:
-            return float(self.bis10kWp) * 0.01 * self.einsp_energie
+            # return float(self.bis10kWp) * 0.01 * self.einsp_energie 
+            return float(self.bis10kWp) * 0.01 * (10 / self.modulsumme_kWp)
         else:
             klZehn = float(self.bis10kWp) * 0.01 * (10 / self.modulsumme_kWp)
             grZehn = (
