365877430cc9b2d11c34a11342ce904ca6738f45
SamTeamIt <teamit@juno-solar.com>
2024-04-29 12:33:55 +0200

    text migrations

---
+++ vertrieb_interface/utils.py
@@ -0,0 +1,24 @@
+import hashlib
+from django.core.exceptions import ValidationError
+
+
+def validate_range(value):
+    if not isinstance(value, int):
+        if value < 0 or value > 6:
+            raise ValidationError(
+                ("Ungültige Eingabe: %(value)s. Der gültige Bereich ist 0-6."),
+                params={"value": value},
+            )
+
+
+def extract_modulleistungWp(model_name):
+    parts = model_name.split()
+    for part in parts:
+        if part.isdigit():
+            return int(part)
+    return None
+
+
+def sanitize_cache_key(key):
+    sanitized_key = hashlib.md5(key.encode()).hexdigest()
+    return sanitized_key
