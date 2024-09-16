c570ef22a2be37faf87bf90e29a279ace81ba747
Catwizzel <fabian.sieber@tu-dortmund.de>
2024-09-16 11:36:21 +0200

    Added quick way to modify initial Vergütung for every user

--- vertrieb_interface/utils.py
+++ vertrieb_interface/utils.py
@@ -1,6 +1,6 @@
 import hashlib
 from django.core.exceptions import ValidationError
-
+from authentication.models import User
 
 def validate_range(value):
     if not isinstance(value, int):
@@ -22,3 +22,10 @@ def extract_modulleistungWp(model_name):
 def sanitize_cache_key(key):
     sanitized_key = hashlib.md5(key.encode()).hexdigest()
     return sanitized_key
+
+
+def changeVerguetung():
+    users = User.objects.all()
+    for user in users:
+        User.objects.filter(zoho_id=user.zoho_id).update(initial_bis10kWp=8.03)
+        User.objects.filter(zoho_id=user.zoho_id).update(initial_bis40kWp=6.95)
